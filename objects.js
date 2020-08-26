8874ee8a200a10512d30e500631a3bad43a0c3bf
glorious-number-5611 <rloiseau99@yahoo.com>
2020-08-26 17:43:29 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+    artistNames: 'songs titles'
+  };
+  
+  function updatePlaylist(object, key, value) {
+    playlist.artistNames2 = ["song titles2"];
+    return playlist
+  }
+
+  function removeFromPlaylist(object, key) {
+    delete playlist.artistNames;
+    return playlist
+  }
